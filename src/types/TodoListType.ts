d618eae66bdf9650a80079cde3d1132991da89dc
Moyin <Moyin@Emmanuels-MacBook-Pro.local>
2023-06-20 21:51:53 +0100

    clean up

--- src/types/TodoListType.ts
+++ src/types/TodoListType.ts
@@ -2,8 +2,4 @@ export interface Todo {
     id?: number;
     title: string;
     task: string;
-
-    onDelete: (id: number) => void;
-    onEdit: (id: number, title: string, task: string)=> void;
-    onSubmit?: ()=> void
 }
